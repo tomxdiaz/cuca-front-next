dcb26c9f4ef92644fb1d53c7cbaf0e99b28d26ce
tomydiaz <tomydiaz739@gmail.com>
2023-10-09 19:15:24 -0300

    mono gde foto baño

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -10,6 +10,9 @@ const StyledCarouselContainer = styled('div')((props: { theme?: any }) => {
     label: StyledCarouselContainer;
     display: flex;
     height: 76vh;
+    ${theme.breakpoints.down('md')} {
+      height: 47.5vh;
+    };
     ${theme.breakpoints.down('sm')} {
       height: 37.5vh;
     };
@@ -33,6 +36,9 @@ const StyledCarouselItem = styled('div')((props: { theme?: any }) => {
   return `
     label: StyledCarouselItem;
     height: 76vh;
+    ${theme.breakpoints.down('md')} {
+      height: 47.5vh;
+    };
     ${theme.breakpoints.down('sm')} {
       height: 37.5vh;
     };
@@ -53,9 +59,7 @@ const StyledImg = styled('img')((props: { theme?: any }) => {
 const BootstrapCarousel = ({ images }: any) => {
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 500px)' });
 
-  const _IMG_SRC = isTabletOrMobile
-    ? process.env.NEXT_PUBLIC_IMG_SRC + '/mobile'
-    : process.env.NEXT_PUBLIC_IMG_SRC + '/desktop';
+  const _IMG_SRC = isTabletOrMobile ? process.env.NEXT_PUBLIC_IMG_SRC + '/mobile' : process.env.NEXT_PUBLIC_IMG_SRC + '/desktop';
   return (
     <>
       <StyledCarouselContainer>
@@ -63,10 +67,7 @@ const BootstrapCarousel = ({ images }: any) => {
           {images.map((image: any, i: number) => {
             return (
               <StyledCarouselItem className={'carousel-item'} key={i}>
-                <StyledImg
-                  className='d-block w-100'
-                  src={`${_IMG_SRC}/${image}`}
-                />
+                <StyledImg className='d-block w-100' src={`${_IMG_SRC}/${image}`} />
               </StyledCarouselItem>
             );
           })}
